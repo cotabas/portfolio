c35ff3a29ca7c58fe777c882c9ba903adfb4a7dc
cotabas <cotabas@gmail.com>
2023-03-19 21:11:49 -0700

    letters seperated, and correctly spaced. hover opacity effect worx

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 import './style.css'
+
+const words = window.document.getElementsByClassName('line');
+
+const leterize = element => {
+  const text = element.innerText.split("");
+  element.innerText = "";
+console.log(text);
+  text.forEach(letter => {
+    const span = document.createElement("span");
+
+    span.className = "letter"
+
+    span.innerText = letter;
+
+    element.appendChild(span);
+  });
+}
+console.log(words);
+Array.from(words).forEach(word => {
+  leterize(word);
+});
+
