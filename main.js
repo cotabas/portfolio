6e4ab048ef46a47cb3ed713a43b882b30050ca3c
cotabas <cotabas@gmail.com>
2023-03-20 20:22:09 -0700

    letters popping off

--- main.js
+++ main.js
@@ -1,23 +1,63 @@
 import './style.css'
 
 const words = window.document.getElementsByClassName('line');
+const centerText = window.document.getElementById("center_text");
 
 const leterize = element => {
   const text = element.innerText.split("");
   element.innerText = "";
-console.log(text);
+
   text.forEach(letter => {
     const span = document.createElement("span");
-
     span.className = "letter"
-
     span.innerText = letter;
 
     element.appendChild(span);
   });
 }
-console.log(words);
 Array.from(words).forEach(word => {
   leterize(word);
 });
 
+const letters = window.document.getElementsByClassName("letter");
+
+Array.from(letters).forEach(letter => {
+  letter.addEventListener("mouseover", (event) => {
+    const span = document.createElement("span");
+    span.className = "looseLetter";
+    span.innerText = letter.innerText;
+
+    var rect = letter.getBoundingClientRect();
+    span.style = `top: ${rect.top}px; left: ${rect.left}px;`;
+
+    centerText.appendChild(span);
+
+    span.animate(
+      [
+        { transform: "translate(-200%, -200%)" },
+      ],
+      {
+        duration: 1000,
+        iterations: 1,
+      }
+    );
+
+    var spanRect = span.getBoundingClientRect();
+    span.style = `top: ${spanRect.top}px; left: ${spanRect.left}px;`;
+
+    span.animate(
+      [
+        { transform: "translate(-200%, 200%)" },
+      ],
+      {
+        duration: 1000,
+        iterations: 1,
+      }
+    );
+
+    setTimeout(() => {
+      span.remove();
+    }, "1800");
+  });
+
+});
