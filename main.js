1be75d4b6293300914a743330bab23af4a221746
cotabas <cotabas@gmail.com>
2023-03-22 11:24:24 -0700

    letters looking dope

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import './style.css'
 
 const words = window.document.getElementsByClassName('line');
 const centerText = window.document.getElementById("center_text");
+var rlCount = 0;
 
 const leterize = element => {
   const text = element.innerText.split("");
@@ -31,26 +32,22 @@ function letterMove(letter) {
     span.innerText = letter.innerText;
 
     var rect = letter.getBoundingClientRect();
-    span.style = `top: ${rect.top + 50}px; left: ${rect.left}px;`;
+    span.style = `top: ${rect.top}px; left: ${rect.left}px;`;
 
+    if (rlCount == 0) {
+    span.classList.add("moveUpLeft");
+    span.classList.remove("moveUpLeft");
+    span.classList.add("moveDownLeft");
+    rlCount = 1
+    }else {
+    span.classList.add("moveUpRight");
+    span.classList.remove("moveUpRight");
+    span.classList.add("moveDownRight");
+    rlCount = 0
+    }
     centerText.appendChild(span);
-
-    var letterKeyFrame = new KeyframeEffect(
-      span,
-      [
-        { transform: "translate(-50%, -50%)" },
-      ],
-      {
-        duration: 500,
-      }
-    );
-
-    var letterAnimation = new Animation(letterKeyFrame, document.timeline);
-
-    letterAnimation.play();
-
 
     setTimeout(() => {
       span.remove();
-    }, "1800");
+    }, "2000");
 }
