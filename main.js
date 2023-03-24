b64f9a469d701bd793c7e8b43d4a005e0481ade9
cotabas <cotabas@gmail.com>
2023-03-23 20:49:03 -0700

    curtain lifts

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import './style.css'
 
 const words = window.document.getElementsByClassName('line');
 const centerText = window.document.getElementById("center_text");
+const curtainToggles = window.document.getElementsByClassName("curtainToggle");
 var rlToggle = 0;
 
 const leterize = element => {
@@ -23,29 +24,36 @@ Array.from(words).forEach(word => {
 const letters = window.document.getElementsByClassName("letter");
 Array.from(letters).forEach(letter => {
   letter.addEventListener("mouseover", (event) => { 
-    const span = document.createElement("span");
-    span.className = "looseLetter";
-    span.innerText = letter.innerText;
+    if (document.body.dataset.curtain == "false") { 
+      const span = document.createElement("span");
+      span.className = "looseLetter";
+      span.innerText = letter.innerText;
 
-    var rect = letter.getBoundingClientRect();
-    span.style = `top: ${rect.top}px; left: ${rect.left}px;`;
+      var rect = letter.getBoundingClientRect();
+      span.style = `top: ${rect.top}px; left: ${rect.left}px;`;
 
-    if (rlToggle == 0) {
-      span.classList.add("moveUpLeft");
-      span.classList.remove("moveUpLeft");
-      span.classList.add("moveDownLeft");
-      rlToggle = 1
-    }else {
-      span.classList.add("moveUpRight");
-      span.classList.remove("moveUpRight");
-      span.classList.add("moveDownRight");
-      rlToggle = 0
+      if (rlToggle == 0) {
+        span.classList.add("moveUpLeft");
+        span.classList.remove("moveUpLeft");
+        span.classList.add("moveDownLeft");
+        rlToggle = 1
+      }else {
+        span.classList.add("moveUpRight");
+        span.classList.remove("moveUpRight");
+        span.classList.add("moveDownRight");
+        rlToggle = 0
+      }
+      centerText.appendChild(span);
+
+      setTimeout(() => {
+        span.remove();
+      }, "3000");
     }
-    centerText.appendChild(span);
-
-    setTimeout(() => {
-      span.remove();
-    }, "3000");
   });
 });
 
+Array.from(curtainToggles).forEach(toggle => {
+  toggle.addEventListener("click", (event) => {
+    document.body.dataset.curtain = document.body.dataset.curtain == "true" ? "false" : "true";
+  });
+});
